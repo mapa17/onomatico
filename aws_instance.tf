b8e6a89d7d5f56eeeec0b632c03b08fa1b89806d
Manuel Pasieka <manuel.pasieka@protonmail.ch>
2022-04-15 17:45:58 +0200

    Adding support for cloud-init waiting

--- aws_instance.tf
+++ aws_instance.tf
@@ -1,24 +1,40 @@
+# User variables, path to private and public key used with AWS
+variable "prv_key_path" {
+  default = "~/.ssh/aws_dev"
+}
+
+variable "pub_key_path" {
+  default = "~/.ssh/aws_dev.pub"
+}
+
+variable "instance_type"{
+  #default = "p3.2xlarge"
+  default = "t2.small"
+}
+
+variable "ami"{
+  default = "ami-0b879110efb09396b"
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
+
+
+##################
 
 terraform {
   required_version = ">= 1.0.7"
 }
 
-variable "key_path" {
-  default = "~/.ssh/aws_dev.pub"
-}
-
 resource "aws_key_pair" "pub-key" {
   key_name   = "pub-key"
-  public_key = file("${var.key_path}")
+  public_key = file("${var.pub_key_path}")
 }
 
 resource "aws_instance" "instance" {
-  ami                         = "ami-0b879110efb09396b"
-  #instance_type               = "p3.2xlarge"
-  instance_type               = "t2.small"
+  ami                         = "${var.ami}"
+  instance_type               = "${var.instance_type}"
   key_name                    = aws_key_pair.pub-key.id
   vpc_security_group_ids      = [aws_security_group.sg.id]
   associate_public_ip_address = true
@@ -32,6 +48,23 @@ resource "aws_instance" "instance" {
   tags = {
     Name = "AWS_ML_Instance"
   }
+}
+
+# Our user_data script will be executed as part of cloud-init final
+# Block terraform until cloud-init has finished executing our script and the instance is ready.
+# Create a special null_resource that will use remote-exec to wait until cloud-init has finished
+resource "null_resource" "cloud_init_wait" {
+  connection {
+    host        = "${aws_instance.instance.public_ip}"
+    user        = "ec2-user"
+    private_key = "${file("${var.prv_key_path}")}"
+    #script_path = "/tmp/cloud_init_wait.sh"
+    timeout     = "10m"
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo cloud-init status --wait"]
+  }
+  depends_on = [aws_instance.instance]
 }
 
 resource "aws_security_group" "sg" {
@@ -57,7 +90,6 @@ resource "aws_security_group" "sg" {
     Name = "AWS_ML_DEV_INSTANCE"
   }
 }
-
 
 output "instance-public-ip" {
   value = aws_instance.instance.public_ip
--- setup_instance.sh
+++ setup_instance.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "#### MY SCRIPT !!! ###"
+
 # Log all to file
 exec > /tmp/setup_instance.log                                                                      
 exec 2>&1
@@ -8,7 +10,7 @@ exec 2>&1
 curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /tmp/miniconda.sh
 
 # Install as ec2-user miniconda in batch mode
-sudo -u ec2-user -- bash -c 'cd /home/ec2-user && echo -n "Running as " && who && bash /tmp/miniconda.sh -b && ./miniconda/bin/conda init'
+sudo -u ec2-user -- bash -c 'cd /home/ec2-user && echo -n "Running as " && who && bash /tmp/miniconda.sh -b && ./miniconda/bin/conda init bash'
 
 # Create env with support for poetry (will download a lot of other stuff)
 sudo -u ec2-user -- bash -c 'cd /home/ec2-user && ./miniconda3/bin/conda create -n mldev poetry -y'
